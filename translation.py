dd435154b8fa6681ba2b77ce879f9bb41c41f2f2
Kgyya <76932451+Kgyya@users.noreply.github.com>
2021-03-11 06:28:28 +0700

    translation.py

--- translation.py
+++ translation.py
@@ -1,19 +1,16 @@
 class Translation(object):
-    START_TEXT = """Hi!
-Thank you for using me 😬
-Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
+    START_TEXT = """Halo Om!
+Selamat Datang Di Bot Telegram APP-ID Scrapper 
+Masukkin Nomor, Masukkin OTP, Muncul API Key / Api Hash
 
-/start at any stage to re-enter your details"""
-    AFTER_RECVD_CODE_TEXT = """I see!
-now please send the Telegram code that you received from Telegram!
+Ketik /start Dulu"""
+    AFTER_RECVD_CODE_TEXT = """Nah Ada OTP Kan?
+Kirim Ke Sini OTP Nya
+*Klo Ga Niat Gaush di Kirim Jg Gpp, Bodo
 
-this code is only used for the purpose of getting the APP ID from my.telegram.org
-if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/swatv3nub/ScrapperRobot and clicking on the "Deploy in Heroku" Button
-
-/start at any stage to re-enter your details"""
-    BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@ProjectHackfreaks"
-    CANCELLED_MESG = "Bye! Please re /start the bot conversation"
-    IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
-    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
+Ketik /start Dulu"""
+    BEFORE_SUCC_LOGIN = "Sedang Menganu Web Telegram ..."
+    ERRED_PAGE = "Error! Silahkan Hubungi @Kgyya Untuk Penjelasan Lebih Lanjut"
+    CANCELLED_MESG = "Bye, Ketik /start Lagi Klo Butuh w:v"
+    IN_VALID_CODE_PVDED = "Salah Goblok"
+    IN_VALID_PHNO_PVDED = "Niat Gak Si Ajg"
